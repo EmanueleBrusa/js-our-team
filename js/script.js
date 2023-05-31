2ef780ee5114acb53547427520168f3c72ab9214
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-05-31 15:10:56 +0200

    [MILESTONE 0] array con oggetti completato

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const listaTeam = [
+{
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    fotoProfilo: 'wayne-barnett-founder-ceo.jpg'
+},
+{
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    fotoProfilo: 'angela-caroll-chief-editor.jpg'
+},
+{
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    fotoProfilo: 'walter-gordon-office-manager.jpg'
+},
+{
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    fotoProfilo: 'angela-lopez-social-media-manager.jpg'
+},
+{
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    fotoProfilo: 'scott-estrada-developer.jpg'
+},
+{
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    fotoProfilo: 'barbara-ramos-graphic-designer.jpg'
+},
+]
+
+console.log(listaTeam)
